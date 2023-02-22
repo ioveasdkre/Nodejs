ecd30a51f31fd2acb21e0af153fbeafb3cf31dfa
ERI\ioveasdkre <a96080103A>
2023-02-22 17:56:25 +0800

    Hexschool Nodejs EP04~05 API 調整

--- Hexschool/Example/test/server.ts
+++ Hexschool/Example/test/server.ts
@@ -1,4 +1,16 @@
 import http, { IncomingMessage, ServerResponse } from "http";
+import { v4 as uuidv4 } from "uuid";
+
+const todos = [
+  {
+    id: uuidv4(),
+    title: "今天要刷牙",
+  },
+  {
+    id: uuidv4(),
+    title: "今天要吃早餐",
+  },
+];
 
 // 請求監聽器
 // request 請求
@@ -13,30 +25,33 @@ const requestListenet = (req: IncomingMessage, res: ServerResponse) => {
   };
 
   const setRes = (
-    status: number,
+    title: number,
     headersObj: { [key: string]: string },
-    write: { status: string; data: [] } | { status: string; massage: string }
+    write?: { title: string; data: any[] } | { title: string; massage: string }
   ) => {
     // 設置 HTTP 狀態碼為 200，並設置標頭 headers
-    res.writeHead(status, headersObj);
-    // 回傳 write 字串
-    res.write(JSON.stringify(write));
+    res.writeHead(title, headersObj);
+
+    if (write) res.write(JSON.stringify(write)); // 回傳 write 字串
+
     // 結束回應
     res.end();
   };
 
-  console.log("req.url: ", req.url);
-  console.log("req.method: ", req.method);
+  // console.log("req.url: ", req.url);
+  // console.log("req.method: ", req.method);
 
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/todos" && req.method === "GET") {
     // 如果請求方法為 GET 且路徑為 /，回傳 "index" 字串
-    setRes(200, headers, { status: "success", data: [] });
-  } else if (req.url === "/" && req.method === "DELETE") {
-    // 如果請求方法為 DELETE 且路徑為 /，回傳 "刪除成功" 字串
-    setRes(200, headers, { status: "刪除成功", data: [] });
+    setRes(200, headers, { title: "success", data: todos });
+  } else if (req.method === "OPTIONS") {
+    setRes(200, headers);
   } else {
     // 其他情況，回傳 "not found 404" 字串
-    setRes(404, headers, { status: "not found 404", massage: "無此網路路由" });
+    setRes(404, headers, {
+      title: "not found 404",
+      massage: "無此網路路由",
+    });
   }
 };
 
